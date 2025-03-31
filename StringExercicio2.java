ea492df79a692fac2dfb20fe2422043d2f985edf
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-03-31 19:54:07 -0300

    Create StringExercicio2.java

---
+++ StringExercicio2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+    public static int f_contagem (String palavra){
+        int nvogais = 0;
+        String minusculo = palavra.toLowerCase(); // Põe tudo em minúsculo
+        for (int i = 0; i < minusculo.length(); i++){
+            if (minusculo.charAt(i) == 'a' || minusculo.charAt(i) == 'e' || minusculo.charAt(i) == 'i' || minusculo.charAt(i) == 'o' || minusculo.charAt(i) == 'u'){
+                nvogais = nvogais + 1;
+            }
+        }
+        return nvogais;
+    }    
+
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner (System.in);
+		String palavra = scanner.nextLine();
+		int nvogais = f_contagem(palavra);
+		System.out.printf("A palavra %s contém %d vogais", palavra, nvogais);
+		
+		scanner.close();
+	}
+}
