295c1d5bb998bb2bc05cb3dc0abc1a54d2d2e48c
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-04-04 11:17:45 -0300

    Create StringExercicio5Desafio2.java

---
+++ StringExercicio5Desafio2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class StringExercicio5Desafio2 {
+
+	public static void f_separar (String frase) {
+		int tamanho = frase.length();
+		char letra;
+		String palavra = "";
+		String maiorp = "";
+		String menorp = "";
+
+		int contador = 0;
+
+		for (int i = 0; i < tamanho; i++) {
+			if (frase.charAt(i) != ' ') {
+				palavra = palavra + frase.charAt(i);
+			}
+			else if (palavra.length() >= 4) {
+				System.out.println(palavra);
+
+				if ((menorp.equals("")) || (palavra.length() < menorp.length())) {
+					menorp = palavra;
+				}
+				if (palavra.length() > maiorp.length()) {
+					maiorp = palavra;
+				}
+		    	palavra = "";
+
+			}
+
+		}
+		System.out.printf("MENOR PALAVRA ENCONTRADA: %s", menorp);
+		System.out.println("");
+		System.out.printf("MAIOR PALAVRA ENCONTRADA: %s", maiorp, "\n");
+
+	}
+
+	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		String frase = scanner.nextLine();
+		System.out.println("PALAVRAS ENCONTRADAS");
+		System.out.println("====================");
+		f_separar(frase);
+		scanner.close();
+	}
+
+}
