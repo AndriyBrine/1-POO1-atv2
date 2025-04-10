54cd96561afa443a9be5088095c829415323e298
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-04-09 22:20:14 -0300

    Update StringExercicio5Desafio2.java

--- StringExercicio5Desafio2.java
+++ StringExercicio5Desafio2.java
@@ -54,73 +54,60 @@ public class StringExercicio5Desafio2 {
 
 
 
-V2 PRA CONSERTAR
+V2 CONSERTADA
 
+import java.util.Scanner;
 
+public class StringExercicio5Desafio2 {
 
+    public static void f_separar(String frase) {
+        int tamanho = frase.length();
+        String palavra = "";
+        String maiorp = "";
+        String menorp = "";
 
+        for (int i = 0; i < tamanho; i++) {
+            char letra = frase.charAt(i);
 
+            if (letra != ' ') {
+                palavra += letra;
+            } else {
+                if (palavra.length() >= 4) {
+                    System.out.println(palavra);
 
+                    if (menorp.equals("") || palavra.length() < menorp.length()) {
+                        menorp = palavra;
+                    }
+                    if (palavra.length() > maiorp.length()) {
+                        maiorp = palavra;
+                    }
+                }
+                palavra = "";
+            }
+        }
 
+        // Processa a última palavra (se não terminar com espaço)
+        if (palavra.length() >= 4) {
+            System.out.println(palavra);
 
+            if (menorp.equals("") || palavra.length() < menorp.length()) {
+                menorp = palavra;
+            }
+            if (palavra.length() > maiorp.length()) {
+                maiorp = palavra;
+            }
+        }
 
+        System.out.printf("MENOR PALAVRA ENCONTRADA: %s\n", menorp);
+        System.out.printf("MAIOR PALAVRA ENCONTRADA: %s\n", maiorp);
+    }
 
-
-	import java.util.Scanner;
-public class Main {
-
-	public static void f_separar (String frase) {
-		int tamanho = frase.length();
-		char letra;
-		String palavra = "";
-		String maiorp = "";
-		String menorp = "";
-
-		int contador = 0;
-
-		for (int i = 0; i < tamanho; i++) {
-			if (frase.charAt(i) != ' ') {
-				palavra = palavra + frase.charAt(i);
-			}
-			else {
-			    if (palavra.length() >= 4) {
-				System.out.println(palavra);
-
-				if ((menorp.equals("")) || (palavra.length() < menorp.length())) {
-					menorp = palavra;
-				}
-				if (palavra.length() > maiorp.length()) {
-					maiorp = palavra;
-				}
-		    	palavra = "";
-
-			    }
-		    }
-
-            if (palavra.length() >= 4) {
-				System.out.println(palavra);
-
-				if ((menorp.equals("")) || (palavra.length() < menorp.length())) {
-					menorp = palavra;
-				}
-				if (palavra.length() > maiorp.length()) {
-					maiorp = palavra;
-				}
-			}
-		}
-		System.out.printf("MENOR PALAVRA ENCONTRADA: %s", menorp);
-		System.out.println("");
-		System.out.printf("MAIOR PALAVRA ENCONTRADA: %s", maiorp, "\n");
-
-	}
-
-	public static void main (String args[]) {
-		Scanner scanner = new Scanner(System.in);
-		String frase = scanner.nextLine();
-		System.out.println("PALAVRAS ENCONTRADAS");
-		System.out.println("====================");
-		f_separar(frase);
-		scanner.close();
-	}
-
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        String frase = scanner.nextLine();
+        System.out.println("PALAVRAS ENCONTRADAS");
+        System.out.println("====================");
+        f_separar(frase);
+        scanner.close();
+    }
 }
